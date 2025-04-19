d96046028481dedd85e673620634c7a9affd6bcf
BUMBAIYA <amitchauhan263871@gmail.com>
2025-04-19 14:08:44 +0530

    svg highlight

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,64 @@
-const SVG_NS = "http://www.w3.org/2000/svg";
+const SVG_NS = "http://www.w3.org/2000/svg" as const;
+
+document.getElementById("click")?.addEventListener("click", () => {
+  alert("click through highlight");
+});
 
 const app = document.body;
 
-const config = {
-  width: 400,
-  height: 300,
+const generateHighlight = (
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  padding: number
+) => {
+  const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
+  const _padding = Math.max(padding, 0);
+  return `M0,0 H${windowWidth} V${windowHeight} H0 V0 Z M${x - _padding},${
+    y - _padding
+  } H${x + width + _padding} V${y + height + _padding} H${x - _padding} V${
+    y - _padding
+  } z`;
 };
 
-const generateOverlayPath = (_config = config) => {
-  return `M0,0 L${_config.width},0 L${_config.width},${_config.height} L0,${_config.height} L0,0 Z`;
-};
-
-const createSvg = (_config = config) => {
-  const { width, height } = config;
+const createSvg = (
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  padding: number
+) => {
+  const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
   const svg = document.createElementNS(SVG_NS, "svg");
-  svg.setAttribute("viewBox", `0 0 ${width} ${height}`);
-  svg.setAttribute("width", `${width}`);
-  svg.setAttribute("height", `${height}`);
+  svg.setAttribute("viewBox", `0 0 ${windowWidth} ${windowHeight}`);
 
-  const stagePath = document.createElementNS(SVG_NS, "path");
-  stagePath.setAttribute("d", generateOverlayPath(_config));
-  stagePath.style.fill = "red";
-  stagePath.style.opacity = "0.7";
-  svg.append(stagePath);
+  svg.style.position = "fixed";
+  svg.style.zIndex = "100000";
+  svg.style.inset = "0";
+  svg.style.fillRule = "evenodd";
+  svg.style.clipRule = "evenodd";
+  svg.style.pointerEvents = "none";
+
+  svg.style.fill = "rgb(256,100,0)";
+  svg.style.opacity = "0.7";
+
+  const highlightPath = document.createElementNS(SVG_NS, "path");
+  highlightPath.setAttribute(
+    "d",
+    generateHighlight(x, y, width, height, padding)
+  );
+
+  highlightPath.style.pointerEvents = "auto";
+  highlightPath.style.cursor = "auto";
+  svg.append(highlightPath);
 
   return svg;
 };
 
-app.append(createSvg(config));
+const WIDTH = 300;
+const HEIGHT = 300;
+
+app.append(createSvg(window.innerWidth / 2 - WIDTH / 2, 150, WIDTH, HEIGHT, 5));
